c351b5b3d549ba1e4e6dda49141eedbfd8057748
Chun Ly <astro.chun@gmail.com>
2018-04-01 02:15:22 -0700

    plotting.line_fitting.main: Use OH skyline mask to determine median

--- plotting/line_fitting.py
+++ plotting/line_fitting.py
@@ -16,7 +16,7 @@ from scipy.optimize import curve_fit
 
 from ..extract import gauss1d
 
-def main(wave, spec1d, ax):
+def main(wave, spec1d, OH_arr, ax):
 
     '''
     Main() function for line_fitting
@@ -29,12 +29,21 @@ def main(wave, spec1d, ax):
     spec1d : dict
       Dictionary containing wavelengths and fluxes
     
+    OH_arr : np.array
+      Array of 0 and 1 for where pixel is affected by OH skyline emission
+
+    ax : matplotlib.axes._subplots.AxesSubplot
+      Matplotlib axes from plt.subplots()
+
     Returns
     -------
+    ax with Gaussian fits overlaid
 
     Notes
     -----
     Created by Chun Ly, 1 April 2018
+     - Use OH skyline mask to determine median
+     - Set boundaries for curve_fit fitting
     '''
 
     lamb0 = spec1d['wave']
@@ -42,19 +51,25 @@ def main(wave, spec1d, ax):
 
     n_lines = len(wave)
 
+    unmask = np.where(OH_arr == 0)[0]
+    med0 = np.median(flux0[unmask])
+    #ax.axhline(y=med0,color='magenta')
+
     for ii in range(n_lines):
         idx = [xx for xx in range(len(lamb0)) if
                np.abs(lamb0[xx]-wave[ii]) < 1.0]
         max0 = max(flux0[idx])
-        p0 = [0.0, max0, wave[ii], 0.4]
-        popt, pcov = curve_fit(gauss1d, lamb0, flux0, p0=p0)
+        p0 = [med0, max0, wave[ii], 0.4]
+        param_bounds = ((0.995*med0, 0.9*max0, wave[ii]-0.2, 0),
+                        (1.005*med0, 1.1*max0, wave[ii]+0.2, np.inf))
+
+        popt, pcov = curve_fit(gauss1d, lamb0, flux0, p0=p0, bounds=param_bounds)
         center0 = popt[2]
         sigma0  = popt[3]
-        print popt
         sig_idx = np.where(np.abs(lamb0-center0)/sigma0 <= 2.5)[0]
         int_flux = np.sum(flux0[sig_idx] - popt[0])
         print ii, wave[ii], center0, sigma0, int_flux
-        ax.plot(lamb0, gauss1d(lamb0, *popt))
+        ax.plot(lamb0, gauss1d(lamb0, *popt), color='g')
 
     return ax
 #enddef
